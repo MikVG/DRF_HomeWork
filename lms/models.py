cd19d1a21d85d0d2c16e7ba02a57d22b688cdc4b
Mikhail Glushkov <miggl@mail.ru>
2024-04-13 20:27:58 +0300

    create model course and lesson

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    preview = models.ImageField(upload_to='lms/course/', verbose_name='превью', null=True, blank=True)
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='lms/lesson/', verbose_name='превью', null=True, blank=True)
+    video_link = models.URLField(max_length=250, verbose_name='ссылка на видео', null=True, blank=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
