e6e1771411743d1c05f55a052b706e327b501f94
Mikhail Glushkov <miggl@mail.ru>
2024-04-16 19:35:05 +0300

    24.2 Task 1 done

--- lms/serializers.py
+++ lms/serializers.py
@@ -4,10 +4,14 @@ from lms.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lesson_count(self, instance):
+        return instance.lesson_set.count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
