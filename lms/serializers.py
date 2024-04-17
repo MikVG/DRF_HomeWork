e2de5ee8ace28e57e489513e9170463a59c63228
Mikhail Glushkov <miggl@mail.ru>
2024-04-17 20:08:04 +0300

    24.2 Task 3 done

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,8 +3,16 @@ from rest_framework import serializers
 from lms.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
 
     class Meta:
         model = Course
@@ -12,10 +20,3 @@ class CourseSerializer(serializers.ModelSerializer):
 
     def get_lesson_count(self, instance):
         return instance.lesson_set.count()
-
-
-class LessonSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Lesson
-        fields = '__all__'
